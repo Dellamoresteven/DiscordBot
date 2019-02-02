b5f178533d9b48d55de8f9208b77e65316e0b7b2
Dellamoresteven <dellamoresteven@gmail.com>
2019-02-02 11:45:30 -0500

    Test Commit

--- BotMain.java
+++ BotMain.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
-class BotMain{
+/* API token for discord */
 
-  
+
+class BotMain{
+  public static void main(String args[]){
+    String DiscordToken = "NTQxMjk2NTI4NzI0MTk3Mzc2.DzdZkg.l3NYwdtY1RJwfQJ3-U9Vxz61LWU";
+    System.out.println("This is a test to make sure everyhting works\nToken: " + DiscordToken);
+  }
 }
